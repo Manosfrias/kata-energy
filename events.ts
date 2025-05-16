a6d1491ade60d2221815f9055aeed06011acc6f1
Ana Manosfrias <a.gilamor@gmail.com>
2025-05-16 14:18:00 +0200

    Update events.ts

--- events.ts
+++ events.ts
@@ -1,10 +1,11 @@
 export const events = [
   { name: "Meetup de frontend", type: "tech", duration: 2, energyCost: 5, priority: 8, day: "Monday" },
-  { name: "Cena con ex-compañeres del bootcamp", type: "social", duration: 3, energyCost: 7, priority: 6, day: "Tuesday" },
+  { name: "Cena con amigos", type: "social", duration: 3, energyCost: 7, priority: 6, day: "Tuesday" },
   { name: "Tarde de descanso (sofá y manta)", type: "rest", duration: 3, energyCost: 0, priority: 9, day: "Wednesday" },
   { name: "Afterwork de la oficina", type: "social", duration: 2, energyCost: 6, priority: 5, day: "Thursday" },
-  { name: "Taller de Git emocional", type: "tech", duration: 1, energyCost: 3, priority: 7, day: "Thursday" },
+  { name: "Taller de Cosas Griegas, por Gonzalo", type: "tech", duration: 1, energyCost: 8, priority: 2, day: "Thursday" },
   { name: "Vermú con amigues", type: "social", duration: 2, energyCost: 4, priority: 8, day: "Saturday" },
-  { name: "Charla sobre burnout en tech", type: "tech", duration: 1, energyCost: 4, priority: 6, day: "Sunday" },
+  { name: "Noche de party", type: "social", duration: 10, energyCost: 8, priority: 10, day: "Saturday" },
+  { name: "Expo en CentroCentro", type: "social", duration: 1, energyCost: 4, priority: 6, day: "Sunday" },
   { name: "Domingo en casa sin hablar con nadie", type: "rest", duration: 4, energyCost: 0, priority: 10, day: "Sunday" },
 ];
