911d156a45f4c8d07c43c8daf4d69943a4e4e732
Ana Manosfrias <a.gilamor@gmail.com>
2025-05-17 11:14:42 +0200

    Update events-extra.ts

--- src/events-extra.ts
+++ src/events-extra.ts
@@ -2,5 +2,5 @@ export const extraEvents = [
   { name: "Hackathon con jugoso premio", type: "tech", duration: 6, energyCost: 7, priority: 10, day: "Saturday" },
   { name: "Cita tinder!!", type: "social", duration: 3, energyCost: 4, priority: 9, day: "Friday" },
   { name: "Presentación del libro de Mánu", type: "tech", duration: 1, energyCost: 2, priority: 10, day: "Wednesday" },
-  { name: "Paseo de introspección: ¿realmente debería parar un poco?", type: "rest", duration: 2, energyCost: 0, priority: 7, day: "Saturday" }
+  { name: "Paseo de introspección: ¿realmente debería parar un poco?", type: "rest", duration: 2, energyCost: 0, priority: 7, day: "" }
 ];
